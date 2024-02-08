f653b8105eb378dbfed179a09a54e9d0503b3854
eliteabhi <abhirangarajan2003@gmail.com>
2024-02-07 23:51:59 -0600

    Made `main.c` be more descriptive

---
+++ week3/static-libraries/README.md
@@ -0,0 +1,12 @@
+# Notes
+    
+    - `libs.tar` contains the two **lib_** directories
+    - I used `alpine:latest` as my base image, due to it's small size
+    - `build-base` is the Alpine equivalent of `build-essential`
+
+---
+
+# Links
+    - Github Repo URL: https://github.com/eliteabhi/embedded-assignments/tree/main
+    - Github Repo SSH Address: git@github.com:eliteabhi/embedded-assignments.git
+    - Docker Hub Image: eliteabhi/static-libs:latest
--- week3/static-libraries/main.c
+++ week3/static-libraries/main.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 int main() {
 
-    printf( "%d\n", add(2, 2) );
-    printf( "%d\n", sub(2, 2) );
-    printf( "%d\n", mul(2, 2) );
-    printf( "%d\n", div(2, 2) );
+    printf( "2 + 2 = %d\n", add(2, 2) );
+    printf( "2 - 2 = %d\n", sub(2, 2) );
+    printf( "2 * 2 = %d\n", mul(2, 2) );
+    printf( "2 / 2 = %d\n", div(2, 2) );
 
-    printf( "%0.3f\n", f_add(2.0, 3.0) );
-    printf( "%0.3f\n", f_sub(2.0, 3.0) );
-    printf( "%0.3f\n", f_mul(2.0, 3.0) );
-    printf( "%0.3f\n", f_div(2.0, 3.0) );
+    printf( "2.0 + 3.0 = %0.3f\n", f_add(2.0, 3.0) );
+    printf( "2.0 - 3.0 = %0.3f\n", f_sub(2.0, 3.0) );
+    printf( "2.0 * 3.0 = %0.3f\n", f_sub(2.0, 3.0) );
+    printf( "2.0 / 3.0 = %0.3f\n", f_div(2.0, 3.0) );
 
     return 0;
 
