049dc3d3187e42bd3cb3b405c3cb09ca59b2d7bf
eliteabhi <abhirangarajan2003@gmail.com>
2024-03-02 17:40:58 -0600

    Fixed script a bit

--- week5/cross-compiling/duo_sdk_build.sh
+++ week5/cross-compiling/duo_sdk_build.sh
@@ -3,84 +3,45 @@
 CC_IMAGE_NAME="eliteabhi/duo-sdk:latest"
 CWD=$(pwd)
 IFS=" "
-COMMAND="$@"
-BUILD_COMMAND="bash -c \""
+COMMAND="$*"
+BUILD_COMMAND=""
 
-# See if element is in list
-contains() {
+echo ${COMMAND}
 
-  local list="$1"
-  local item="$2"
+while getopts i:p: OPTION; do
 
-  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
-    # yes, list include item
-    result=0
-  else
-    result=1
-  fi
-  return $result
+  case "${OPTION}" in
 
-}
-
-find_index() {
-
-  local list="$1"
-  local item="$2"
-
-  for i in "${!list[@]}"; do
-   if [[ "${list[$i]}" = "${item}" ]]; then
-       echo ${i};
-       break;
-   fi
-  done
-
-}
-
-if contains COMMAND "--image"; then
-
-  image_arg_index=$(($(find_index COMMAND "--image") + 1))
-
-  CC_IMAGE_NAME="$COMMAND[$image_arg_index]"
-
-  delete_flag_arg="--image"
-  delete_img_arg="$COMMAND[$image_arg_index]"
-
-  COMMAND="${COMMAND[@]/$delete_flag_arg}"
-  COMMAND="${COMMAND[@]/$delete_img_arg}"
-
-fi
-
-if contains COMMAND "--pull-image" || contains COMMAND "-p"; then 
-
-  sudo docker pull ${CC_IMAGE_NAME}
-
-  COMMAND="${COMMAND[@]/$delete_flag_arg}"
-
-  delete_flag_arg="--pull-image"
-
-  COMMAND="${COMMAND[@]/$delete_flag_arg}"
-
-fi
-
-if contains COMMAND "cmake"; then
-
-  BUILD_COMMAND="${BUILD_COMMAND}rm -rf /app/*CMake* && "
-
-  if contains COMMAND ".."; then
-
-    BUILD_COMMAND="${BUILD_COMMAND}if [[ -d /app/build ]]; then rm -rf /app/build; fi && /app/build && mkdir -p /app/build && cd /app/build && ${COMMAND}\""
+    i)
+      CC_IMAGE_NAME="${OPTARG}";;
     
-  fi
+    p)
+      sudo docker pull ${CC_IMAGE_NAME}
 
-  if contains COMMAND "--build"; then
+  esac
+done
 
-    BUILD_COMMAND="cd /app/build && ${COMMAND}\""
+if [[ "$COMMAND" == *"cmake"* ]]; then
+
+  BUILD_COMMAND="rm -rf /app/*CMakeCache* && "
+
+  if [[ "$COMMAND" == *".."* ]]; then
+
+    BUILD_COMMAND="${BUILD_COMMAND}if [[ -d /app/build ]]; then rm -rf /app/build; fi && mkdir -p /app/build && cd /app/build && ${COMMAND}"  
+
+  elif [[ "$COMMAND" == *"--build"* ]]; then
+
+    BUILD_COMMAND="${BUILD_COMMAND}cd /app/build && ${COMMAND}"
+
+  else
+
+    BUILD_COMMAND="${BUILD_COMMAND}${COMMAND}"
 
   fi
 
 else
 
-  BUILD_COMMAND="${BUILD_COMMAND}${COMMAND}\""
+  BUILD_COMMAND="${COMMAND}"
 
 fi
 
@@ -88,20 +49,22 @@ echo
 echo "Build Command: ${BUILD_COMMAND}"
 echo
 
-echo "Full Command: sudo docker run --rm -v \"${CWD}\":/app -t ${CC_IMAGE_NAME} ${BUILD_COMMAND}"
+echo "Full Command: sudo docker run --rm -v \"${CWD}\":/app -t ${CC_IMAGE_NAME} /bin/bash -c \"${BUILD_COMMAND}\""
 echo
 
-echo "Is the command correct?[y/N]: "
-read -sn 1 correct
-
+read -sn 1 -p "Is the command correct?[y/N]: " correct
+echo
 echo
 
-if [[ $correct == "y" ]]; then
+if [[ "$correct" == "y" ]]; then
 
-  sudo docker run --rm -v "${CWD}":/app -t ${CC_IMAGE_NAME} ${BUILD_COMMAND}
+  sudo docker run --rm -v "${CWD}":/app -t ${CC_IMAGE_NAME} /bin/bash -c "${BUILD_COMMAND}"
+  ERROR=$?
 
 fi
 
+echo
 echo "Have a nice day!!"
-exit 0
+
+exit $ERROR
 
