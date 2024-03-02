4cb904d1081636c23d644470ae8cbfa4b52926d6
eliteabhi <abhirangarajan2003@gmail.com>
2024-03-01 21:27:28 -0600

    aHHhhhhH

--- week5/cross-compiling/duo_sdk_build.sh
+++ week5/cross-compiling/duo_sdk_build.sh
@@ -4,12 +4,14 @@ CC_IMAGE_NAME="eliteabhi/duo-sdk:latest"
 CWD=$(pwd)
 IFS=" "
 COMMAND="$@"
-BUILD_COMMAND=""
+BUILD_COMMAND="bash -c \""
 
 # See if element is in list
 contains() {
+
   local list="$1"
   local item="$2"
+
   if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
     # yes, list include item
     result=0
@@ -17,29 +19,60 @@ contains() {
     result=1
   fi
   return $result
+
 }
 
+find_index() {
+
+  local list="$1"
+  local item="$2"
+
+  for i in "${!list[@]}"; do
+   if [[ "${list[$i]}" = "${item}" ]]; then
+       return ${i};
+   fi
+  done
+
+}
+
+if contains COMMAND "--image"; then
+
+  local image_arg_index=$(find_index COMMAND "--image") + 1
+
+  CC_IMAGE_NAME="$COMMAND[$image_arg_index]"
+
+fi
 
 if contains COMMAND "cmake"; then
 
+  BUILD_COMMAND="${BUILD_COMMAND}rm -rf /app/*CMake* && "
+
   if contains COMMAND ".."; then
 
-    BUILD_COMMAND="bash -c \"mkdir -p /app/build && cd /app/build && $COMMAND\""
+    BUILD_COMMAND="${BUILD_COMMAND}if [[ -d /app/build ]]; then rm -rf /app/build; fi && /app/build && mkdir -p /app/build && cd /app/build && ${COMMAND}\""
     
   fi
 
   if contains COMMAND "--build"; then
 
-    BUILD_COMMAND="bash -c \"cd /app/build && $COMMAND\""
+    BUILD_COMMAND="cd /app/build && ${COMMAND}\""
 
   fi
 
 else
 
-  BUILD_COMMAND=$COMMAND
+  BUILD_COMMAND="${BUILD_COMMAND}${COMMAND}\""
 
 fi
 
-sudo docker pull $CC_IMAGE_NAME
-sudo docker run -v "$CWD:/app" $CC_IMAGE_NAME $BUILD_COMMAND
+if contains COMMAND "--pull-image" || contains "-p"; then 
+
+  sudo docker pull ${CC_IMAGE_NAME}
+
+fi
+
+echo
+echo "Build Command: ${BUILD_COMMAND}"
+echo
+sudo docker run -v "${CWD}:/app" ${CC_IMAGE_NAME} ${BUILD_COMMAND}
 
