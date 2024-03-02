c435a053a3526355fa0edab94424e262b9eeb1be
eliteabhi <abhirangarajan2003@gmail.com>
2024-03-01 20:08:35 -0600

    Build script is done (For now)

--- week5/cross-compiling/duo_sdk_build.sh
+++ week5/cross-compiling/duo_sdk_build.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-CC_IMAGE_NAME="eliteabhi/duo-sdk"
-CWD=$(PWD)
+CC_IMAGE_NAME="eliteabhi/duo-sdk:latest"
+CWD=$(pwd)
 IFS=" "
-COMMAND="$*"
+COMMAND="$@"
 BUILD_COMMAND=""
 
+# See if element is in list
+contains() {
+  local list="$1"
+  local item="$2"
+  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
+    # yes, list include item
+    result=0
+  else
+    result=1
+  fi
+  return $result
+}
 
-if [[ $COMMAND == *"cmake"* ]]; then
 
-  if [[ $COMMAND == *".."* ]]; then
+if contains COMMAND "cmake"; then
+
+  if contains COMMAND ".."; then
 
     BUILD_COMMAND="bash -c \"mkdir -p /app/build && cd /app/build && $COMMAND\""
     
   fi
 
-  if [[ $COMMAND == *"--build"* ]]; then
+  if contains COMMAND "--build"; then
 
     BUILD_COMMAND="bash -c \"cd /app/build && $COMMAND\""
 
@@ -27,5 +40,6 @@ else
 
 fi
 
-sudo docker run -v $CWD:/app $CC_IMAGE_NAME $BUILD_COMMAND
+sudo docker pull $CC_IMAGE_NAME
+sudo docker run -v "$CWD:/app" $CC_IMAGE_NAME $BUILD_COMMAND
 
