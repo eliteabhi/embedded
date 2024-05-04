5da95f14abe1b9248dd946b5656974cb97bf356c
eliteabhi <abhirangarajan2003@gmail.com>
2024-05-03 23:38:33 -0500

    Some Refactoring

--- Final-Project/subscriber.c
+++ Final-Project/subscriber.c
@@ -13,13 +13,15 @@
 
 void message_callback(struct mosquitto *mosq, void *userdata, const struct mosquitto_message *message)
 {
-    ssd1306_oled_clear_screen();
+    ssd1306_oled_clear_screen(); // Clear OLED screen
+
     if (message->payloadlen)
     {
         printf("%s %s\n", message->topic, (char *)message->payload);
         cJSON *root = cJSON_Parse(message->payload);
         if (root)
         {
+            // 'name' case
             const cJSON *name = cJSON_GetObjectItemCaseSensitive(root, "name");
             if (cJSON_IsString(name) && (name->valuestring != NULL)) {
 
@@ -27,6 +29,15 @@ void message_callback(struct mosquitto *mosq, void *userdata, const struct mosqu
 
             }
 
+            else {
+
+                printf("Name value is not a string: %s\n", name->valuestring); // Invalid type
+
+            }
+
+            // ----------------------------------------------------------------
+
+            // 'number' case
             const cJSON *number = cJSON_GetObjectItemCaseSensitive(root, "number");
             if (cJSON_IsNumber(number)) {
 
@@ -34,40 +45,73 @@ void message_callback(struct mosquitto *mosq, void *userdata, const struct mosqu
 
             }
 
+            else {
+
+                printf("Number value is not a number: %s\n", number->valueint); // Invalid type
+
+            }
+
+            // ----------------------------------------------------------------
+
+            // 'string_msg' case
             const cJSON *string_msg = cJSON_GetObjectItemCaseSensitive(root, "string_msg");
-            print_message_on_oled( string_msg );
+            if (cJSON_IsString(string_msg)) {
 
+                print_message_on_oled( string_msg ); // Print to OLED screen
 
+            }
+
+            else {
+
+                printf("string_msg value is not a string: %s\n", string_msg->valuestring); // Invalid type
+
+            }
+
+            // ----------------------------------------------------------------
+
+            // 'int_msg' case
             const cJSON *int_msg = cJSON_GetObjectItemCaseSensitive(root, "int_msg");
-            print_message_on_oled( int_msg );
+            if (cJSON_IsNumber(int_msg)) {
+
+                print_message_on_oled( int_msg ); // Print to OLED screen
+
+            }
             
+            else {
 
+                printf("int_msg value is not a number: %s\n", int_msg->valueint); // Invalid type
+
+            }
+
+            // ----------------------------------------------------------------
+
+            // 'task' case
             const cJSON *task = cJSON_GetObjectItemCaseSensitive(root, "task");
-            if ( cJSON_IsString(task) ) {
+            if ( cJSON_IsString(task) ) { // If value is a string
 
-                cJSON* result = cJSON_CreateObject();
+                cJSON* result = cJSON_CreateObject(); // Create empty json object
                 
-                char* task_str = task->valuestring;
+                char* task_str = task->valuestring; // Get task as string
 
-                if ( strstr( task_str, "temperature" ) != NULL ) {
+                if ( strstr( task_str, "temperature" ) != NULL ) { // If task contains 'temperature'
 
-                    struct bmp280_i2c bmp = read_temp_pressure();
+                    struct bmp280_i2c bmp = read_temp_pressure(); // Get values from BMP280
 
-                    cJSON_AddNumberToObject( result, "temperature", bmp.temperature );
+                    cJSON_AddNumberToObject( result, "temperature", bmp.temperature ); // Add 'temperature' item to result object
 
                 }
 
-                else if ( strstr( task_str, "pressure" ) != NULL ) {
+                else if ( strstr( task_str, "pressure" ) != NULL ) { // If task contains 'pressure'
 
-                    struct bmp280_i2c bmp = read_temp_pressure();
+                    struct bmp280_i2c bmp = read_temp_pressure(); // Get values from BMP280
 
-                    cJSON_AddNumberToObject( result, "pressure", bmp.pressure );
+                    cJSON_AddNumberToObject( result, "pressure", bmp.pressure ); // Add 'pressure' item to result object
 
                 }
 
                 else {
 
-                    printf("INVALID TASK\n");
+                    printf("INVALID TASK\n"); // Invalid task
 
                 }
 
@@ -82,76 +126,96 @@ void message_callback(struct mosquitto *mosq, void *userdata, const struct mosqu
 
             }
 
+            else {
+
+                printf( "Task value is not a string: %s\n", task->valuestring );  // Invalid type
+
+            }
+
+            // ----------------------------------------------------------------
+
+            // 'morse' case
             const cJSON *morse = cJSON_GetObjectItemCaseSensitive(root, "morse");
-            if ( cJSON_IsString( morse ) ) {
+            if ( cJSON_IsString( morse ) ) { // If values is a string
 
-                char* morse_str = $( morse->valuestring );
+                char* morse_str = $( morse->valuestring ); // Get morse code as string
 
-                print_message_on_oled( morse );
+                print_message_on_oled( morse ); // Print to OLED screen
 
-                printf( "Blinking Builtin LED...\n" );
-                
-                morse_blink_led( morse_str );
+                printf( "Blinking Builtin LED...\n" ); // Log to console that LED will blink
+
+                morse_blink_led( morse_str ); // Blink builtin LED
+
+            }
+
+            else {
+
+                printf( "Morse value is not a string: %s\n", morse->valuestring ); // Invalid type
 
             }
 
             cJSON_Delete(root);
+        
         }
+        
         else
         {
             printf("Error before: [%s]\n", cJSON_GetErrorPtr());
         }
+
     }
+
     else
     {
         printf("%s (null)\n", message->topic);
     }
+    
 }
 
-void print_message_on_oled( cJSON *message ) {
+void print_message_on_oled( cJSON *message ) { // Prints message onto OLED screen
 
-    char* label = message->string;
+    char* label = message->string; // Get key 'label' from json object
 
-    strcat( label, ": " );
+    strcat( label, ": " ); // Append ': '
 
-    ssd1306_oled_write_string( 0, label );
+    ssd1306_oled_write_string( 0, label ); // Print 'label' onto OLED screen
 
-    if ( cJSON_IsString( message ) ) {
+    if ( cJSON_IsNumber( message ) ) { // If value is a double
 
-        float display_num = message->valuedouble;
+        double display_num = message->valuedouble; // Get value from json object
 
-        char display_num_as_str[2];
+        char display_num_as_str[2]; // NULL string object of length 1 + '\0'
 
-        if ( fmod( display_num, 1.0 ) != 0.0 ) {
+        if ( fmod( display_num, 1.0 ) != 0.0 ) { // If not integer
 
-            sprintf( display_num_as_str, "%f", display_num );
+            sprintf( display_num_as_str, "%f", display_num ); // Cast as float
 
         }
 
         else {
 
-            sprintf( display_num_as_str, "%d", (int) display_num );
+            sprintf( display_num_as_str, "%d", (int) display_num ); // Cast as int
         
         }
 
 
-        printf("%s%s\n", label, display_num_as_str);
+        printf("%s%s\n", label, display_num_as_str); // Log to terminal
 
-        ssd1306_oled_write_string( 0, display_num_as_str );
-
-    }
-
-    else if ( cJSON_IsString( message ) ) {
-
-        char* display_str = &( message->valuestring );
-
-        printf("%s%s\n", label, display_str );
-
-        ssd1306_oled_write_string( 0, display_str );
+        ssd1306_oled_write_string( 0, display_num_as_str ); // Print value to OLED screen
 
     }
 
-    ssd1306_oled_write_string( 0, "\n" );
+    else if ( cJSON_IsString( message ) ) { // If value is a string
+
+        char* display_str = &( message->valuestring ); // Get value from json object
+
+        printf("%s%s\n", label, display_str ); // Log to terminal
+
+        ssd1306_oled_write_string( 0, display_str ); // Print value to OLED screen
+
+    }
+
+    ssd1306_oled_write_string( 0, "\n" ); // Print NewLine to OLED screen
 
 }
 
